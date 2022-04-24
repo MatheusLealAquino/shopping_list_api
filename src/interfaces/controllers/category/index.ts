34bf98dd57df43b0b4c5f5b769810b00612e0992
Matheus Leal <matheusaquino@id.uff.br>
2022-04-23 21:08:17 -0300

    feat: export get categories controller

--- src/interfaces/controllers/category/index.ts
+++ src/interfaces/controllers/category/index.ts
@@ -1,11 +1,19 @@
 import dataAccess from '../../data-access';
 
 import addCategory from '../../../application/usecase/add-category/add-category';
+import getCategories from '../../../application/usecase/get-categories/get-categories';
 
 import makePostCategory from './post-category';
+import makeGetCategories from './get-categories';
 
 const postCategory = (categoryRepository) => (makePostCategory({
 	addCategory: addCategory({
+		categoryRepository,
+	}),
+}));
+
+const getAllCategories = (categoryRepository) => (makeGetCategories({
+	getCategories: getCategories({
 		categoryRepository,
 	}),
 }));
@@ -14,9 +22,11 @@ const categoryController = (repositoryFactory) => {
 	const categoryRepository = dataAccess.categoryMongo(repositoryFactory);
 
 	const postCategoryMounted = postCategory(categoryRepository);
+	const getAllCategoriesMounted = getAllCategories(categoryRepository);
 
 	return Object.freeze({
 		postCategory: postCategoryMounted,
+		getAllCategories: getAllCategoriesMounted,
 	});
 };
 
